18ff53e829db5d5d2eeb9038b9cdcb1008c95dc2
indexedakki <74480055+indexedakki@users.noreply.github.com>
2023-12-19 23:40:17 +0530

    Update list_files.py

--- driver.py
+++ driver.py
@@ -1,25 +1,11 @@
-import itertools
-item=[]
-def combiner(zeros, ones):
-    val=[]
-    n=list(itertools.combinations(range(zeros+ones),ones))
-    p=0
-    for i in range(len(n)):
-        item=['0' for i in range(zeros+ones)]
-        index=n[p]
-        for i in index:
-            item[i]='1'
-        val.append(''.join(item))
-        p+=1
-    return val
-for _ in range(int(input())):
-    n,m=map(int,input().split())
-    val=list(combiner(n,m))
-    cnt=0
-    for i in val:
-        if i[0]=='1':
-            cnt+=1
-    print(cnt)
+import os
+
+def list_files(folder_path):
+    files = []
+    for root, _, filenames in os.walk(folder_path):
+        for filename in filenames:
+            files.append(os.path.join(root, filename))
+    return files
 
 
 
